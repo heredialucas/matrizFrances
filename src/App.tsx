fe79a68f08be0da06c8517386b4731decb18ac79
heredialucas <heredialucasfac22@gmail.com>
2023-09-17 14:26:29 +1200

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import matrizFrances from "../data.js";
+import matrizEng from "../data1.js";
+
 function App() {
   return (
     <div className="App">
@@ -22,6 +24,31 @@ function App() {
                 <th>{user.accion}</th>
                 <th>{user.pronunciacion}</th>
                 <th>{user.traduccion}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <h1>English</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Pronombre</th>
+            <th>Verbo</th>
+            <th>Accion</th>
+            <th className="th1">Pronunciacion</th>
+            <th className="th1">Traducción</th>
+          </tr>
+        </thead>
+        <tbody>
+          {matrizEng.map((user, index) => {
+            return (
+              <tr key={index}>
+                <th>{user.pronoun}</th>
+                <th>{user.verb}</th>
+                <th>{user.action}</th>
+                <th>{user.pronunciation}</th>
+                <th>{user.translation}</th>
               </tr>
             );
           })}
